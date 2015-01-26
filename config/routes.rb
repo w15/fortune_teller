a00d0fb5d0e4317be1817b38a31db4f64707ee9e
Raghu Betina <rbetina@github.com>
2015-01-26 16:06:57 -0600

    Done with dynamic routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,29 +4,12 @@ Rails.application.routes.draw do
   get("/lucky_numbers",   { :controller => "fortunes", :action => "lucky" })
   get("/unlucky_numbers", { :controller => "fortunes", :action => "unlucky" })
 
-  # PART 1: STATIC ROUTES
-  # =====================
-
-  # Uncomment each route below ONE AT A TIME and debug.
-
-  get("/zodiacs/aries",       { :controller => "zodiacs", :action => "ram" })
-  get("/zodiacs/taurus",      { :controller => "zodiacs", :action => "bull" })
-  get("/zodiacs/gemini",      { :controller => "zodiacs", :action => "twins" })
-  get("/zodiacs/cancer",      { :controller => "zodiacs", :action => "crab" })
-  get("/zodiacs/leo",         { :controller => "zodiacs", :action => "lion" })
-  get("/zodiacs/virgo",       { :controller => "zodiacs", :action => "maiden" })
-  get("/zodiacs/libra",       { :controller => "zodiacs", :action => "scales" })
-  get("/zodiacs/scorpio",     { :controller => "zodiacs", :action => "scorpion" })
-  get("/zodiacs/sagittarius", { :controller => "zodiacs", :action => "archer" })
-  get("/zodiacs/capricorn",   { :controller => "zodiacs", :action => "goat" })
-  get("/zodiacs/aquarius",    { :controller => "zodiacs", :action => "waterbearer" })
-  get("/zodiacs/pisces",      { :controller => "zodiacs", :action => "fish" })
-
-
   # PART 2: VARIABLE ROUTES
   # =======================
 
   get("/signs/:the_sign",         { :controller => "zodiacs", :action => "sign" })
+
+  get("/creatures/:the_creature", { :controller => "zodiacs", :action => "creature"})
 
 end
 
